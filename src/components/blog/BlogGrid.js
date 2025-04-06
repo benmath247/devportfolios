ccf0b382029d7a3bc5734c63ebab0e755f17f183
Jonathan Math <jonathanamath@gmail.com>
2025-04-06 15:43:34 -0400

    change styling for blog card

--- src/components/blog/BlogGrid.js
+++ src/components/blog/BlogGrid.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./Card.css";
 
 const BlogGrid = ({ blogSection, colorScheme }) => {
   const { blog_posts, title, description } = blogSection;
-  const { primaryColor, accentColor } = colorScheme;
+  const { accentColor } = colorScheme;
+  const navigate = useNavigate();
+
+  const handleCardClick = (slug) => {
+    navigate(`/blog/${slug}`);
+  };
 
   return (
     <Container>
@@ -14,27 +21,17 @@ const BlogGrid = ({ blogSection, colorScheme }) => {
       <Row xs={1} md={2} lg={3} className="g-4">
         {blog_posts.map((post) => (
           <Col key={post.id}>
-            <Card className="h-100 shadow-sm">
+            <Card eonClick={() => handleCardClick(post.slug)}>
               <Card.Img
                 variant="top"
                 src={post.image}
                 alt={post.title}
-                style={{ height: "200px", objectFit: "cover" }}
+                className="blog-card-img"
               />
               <Card.Body className="d-flex flex-column">
                 <Card.Title>{post.title}</Card.Title>
                 <Card.Text>{post.subtitle}</Card.Text>
-                <Button
-                  href={`/blog/${post.slug}`}
-                  style={{
-                    backgroundColor: primaryColor,
-                    borderColor: primaryColor,
-                    color: "#fff",
-                  }}
-                  className="mt-auto align-self-start"
-                >
-                  Read More
-                </Button>
+                <div className="mt-auto text-primary fw-bold"></div>
               </Card.Body>
             </Card>
           </Col>
