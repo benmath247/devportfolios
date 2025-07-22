e3d772d7e27ffea74a62bd77bec90cfe6f6bd6a1
Jonathan Math <jonathanamath@gmail.com>
2025-07-22 11:29:52 -0400

    add project cards to the ProjectList page

--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -2,51 +2,18 @@ import { Container, Row } from "react-bootstrap";
 import { useSiteConfigContext } from "../contexts/siteConfigContext";
 import ProjectCard from "../components/project/ProjectCard";
 import { Spinner } from "react-bootstrap";
-import ProjectCarousel from "../components/project/ProjectCarousel";
 
 const ProjectList = () => {
   const { siteConfig } = useSiteConfigContext();
   if (!siteConfig) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
-      >
-        <Spinner animation="border" />
-      </div>
-    );
+    return <Spinner animation="border" />;
   }
 
   const projects = siteConfig.projects_block.project_items;
-  const sectionStyle = siteConfig.projects_block.project_style;
 
   if (!projects || projects.length === 0) {
     return <p className="text-center mt-5">No projects to display.</p>;
   }
-  const generateProjects = (projects) => {
-    if (sectionStyle === "carousel") {
-      return (
-        <ProjectCarousel
-          projectsSection={siteConfig.projects_block}
-          colorScheme={siteConfig.color_scheme}
-        />
-      );
-    } else {
-      return (
-        <Row className="justify-content-center">
-          {projects.map((project) => (
-            <ProjectCard project={project} key={project.id} />
-          ))}
-        </Row>
-      );
-    }
-  };
-  console.log("This is working");
-  console.log(sectionStyle);
   return (
     <Container className="py-5">
       <h1
@@ -55,8 +22,11 @@ const ProjectList = () => {
       >
         Projects
       </h1>
-
-      {projects ? generateProjects(projects) : <p>No projects to display.</p>}
+      <Row>
+        {projects.map((project) => (
+          <ProjectCard project={project} key={project.id} />
+        ))}
+      </Row>
     </Container>
   );
 };
